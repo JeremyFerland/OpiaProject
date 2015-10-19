fd264103b0f53fe8e0ed9e0136131b64828f1f55
Marc-Olivier Héroux-Hould <houldmarcolivier@me.com>
2015-10-18 20:42:03 -0400

    Add notes & reference

--- arduino/OpiaArduino/OpiaArduino.ino
+++ arduino/OpiaArduino/OpiaArduino.ino
@@ -1,22 +1,44 @@
+// Created by Jérémy Ferland & Marc-Olivier Héroux-Hould ©2015 V1.0
+// Created on October 16, 2015
+// Arduino V1.6.5-r5
+
+// Description : This code sends data to the serial to control an interactive video projection made on Processing
+// Licence : This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+
+// Title : Opia
+
+//-----------------------------------//
+
+// Analog input
 int pinPot1 = 2;
 int pinPot2 = 3;
+
+// Digital Input
 int pinHall1 = 2;
 
+//-----------------------------------//
 void setup() {
-  // put your setup code here, to run once:
+  // Sets the serial to 57600 bauds
   Serial.begin(57600);
-  pinMode(pinHall1,INPUT);
+  // Read data from the Hall sensor (see: https://www.adafruit.com/products/158)
+  pinMode(pinHall1, INPUT);
 }
 
+//-----------------------------------//
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Print potentiometer #1
   printAnalogVal(pinPot1, "pinPot1");
+  // Print potentiometer #2
   printAnalogVal(pinPot2, "pinPot2");
+  // Print hall sensor #1
   printDigitalVal(pinHall1, "pinHall1");
+  
   delay(100);
 }
 
-void printAnalogVal(int nbCapteur, String prefix){
+//-----------------------------------//
+// Potentiometer print function 
+void printAnalogVal(int nbCapteur, String prefix) {
   int val = analogRead(nbCapteur);
   Serial.print(prefix);
   Serial.print(" ");
@@ -24,7 +46,9 @@ void printAnalogVal(int nbCapteur, String prefix){
   Serial.println();
 }
 
-void printDigitalVal(int nbCapteur, String prefix){
+//-----------------------------------//
+// Hall sensor print function 
+void printDigitalVal(int nbCapteur, String prefix) {
   int val = digitalRead(nbCapteur);
   Serial.print(prefix);
   Serial.print(" ");
